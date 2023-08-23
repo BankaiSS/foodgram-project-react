9e0abe555ff9b5bbe696513c8770c614766ed3b9
BankaiSS <baibanove@mail.ru>
2023-08-23 21:44:40 +0300

    final

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -9,10 +9,12 @@ class IsAdminOrReadOnly(BasePermission):
 
 
 class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user or request.user.is_superuser
 
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        if request.method == 'DELETE':
+            return True
+        return super().has_permission(request, view)
